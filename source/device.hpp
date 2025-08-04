dd3ccd0190a07a3a7c58a806b93f127d6f9bef33
alttq <46207652+alttq@users.noreply.github.com>
2025-08-04 17:14:44 +0200

    Revert "Merge pull request #5 from alttq/codex/modify-sendtocallback-for-frame-queue"
    This reverts commit c7b3fe4147fd7fded03f29903aabd601aacffcbc, reversing
    changes made to 67dd5a2c01ca62d041c6f3c303b3252e4373244c.

--- source/device.hpp
+++ source/device.hpp
@@ -24,26 +24,14 @@
 
 #include <string>
 #include <vector>
-#include <queue>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 using namespace std;
 
 namespace DShow {
 
 struct EncodedData {
-        long long lastStartTime = 0;
-        long long lastStopTime = 0;
-        vector<unsigned char> bytes;
-};
-
-struct FrameData {
-        bool video = false;
-        vector<unsigned char> bytes;
-        long long startTime = 0;
-        long long stopTime = 0;
-        long rotation = 0;
+	long long lastStartTime = 0;
+	long long lastStopTime = 0;
+	vector<unsigned char> bytes;
 };
 
 struct EncodedDevice {
@@ -81,14 +69,8 @@ struct HDevice {
 	bool initialized;
 	bool active;
 
-        EncodedData encodedVideo;
-        EncodedData encodedAudio;
-
-        queue<FrameData> callbackQueue;
-        mutex callbackMutex;
-        condition_variable callbackCond;
-        thread callbackThread;
-        bool callbackStop = false;
+	EncodedData encodedVideo;
+	EncodedData encodedAudio;
 
 	HDevice();
 	~HDevice();
@@ -100,11 +82,9 @@ struct HDevice {
 	bool EnsureActive(const wchar_t *func);
 	bool EnsureInactive(const wchar_t *func);
 
-        inline void SendToCallback(bool video, unsigned char *data, size_t size,
-                                   long long startTime, long long stopTime,
-                                   long rotation);
-
-        void CallbackLoop();
+	inline void SendToCallback(bool video, unsigned char *data, size_t size,
+				   long long startTime, long long stopTime,
+				   long rotation);
 
 	void Receive(bool video, IMediaSample *sample);
 
